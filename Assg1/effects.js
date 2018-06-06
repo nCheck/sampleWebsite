4c03699a8f39a9acb996b1fff2faa46231983e1c
Nehal Kalnad <yenwiikae@gmail.com>
2018-06-06 14:05:52 +0530

    Some additions

--- Assg1/effects.js
+++ Assg1/effects.js
@@ -1,8 +1,9 @@
 
-
+console.log($( window ).width());
 
 	if ($( window ).width() < 600)
 	{
+		var side = $( window ).width();
 		$(".boxleft").removeClass("boxleft")
 			.addClass("padder")
 				.css({"border-bottom": "1px solid grey", "padding-bottom" : "20px" , "padding-top" : "20px" });
@@ -12,11 +13,37 @@
 		$(".boxright").removeClass("boxright")
 			.addClass("padder")
 				.css({"border-bottom": "1px solid grey", "padding-bottom" : "50px" });
+		$('#removenav').css('display' , 'none');
+		$('.product').attr({
+			'height': side-15,
+			'width': side-15
+		}).addClass('img-fluid');
+
+		$('.stickbot').css({
+			'bottom': '0',
+			'positon': 'fixed'
+		});
+		$('.cart').addClass('offset-1');
+		$('.buyNow').addClass('offset-1');
 	}
 
 $('.cbutton').click(function(event) {
-	$('img').attr('src', 'images/' + $(this).attr('id')+'.png');
+	var pid = $(this).attr('id');
+	console.log(pid);
+	console.log($(this).parent('div').parent('div').parent('div').siblings('img').attr('src'));
+	$(this).parent('div').parent('div').parent('div').siblings('img')
+		.attr('src', 'images/' + pid +'.png');
 });
+
+
+$('.cbutton').click(function(event) {
+	var pid = $(this).attr('id');
+	$('img').attr('src', 'images/' + pid +'.png');
+});
+
+
+
+
 
 pincodes = [401300 , 401301 , 401303 , 401305];
 
