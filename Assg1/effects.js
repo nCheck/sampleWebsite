7f99e49737161d43268412f5b73aeeba08917f6a
Nehal Kalnad <yenwiikae@gmail.com>
2018-06-09 11:31:45 +0530

    2

--- Assg1/effects.js
+++ Assg1/effects.js
@@ -69,24 +69,69 @@ $('.ccbutton').click(function(event) {
 pincodes = [401300 , 401301 , 401303 , 401305];
 
 $('#check').click(function(event) {
-	var avail = false;
-	for (var i = pincodes.length - 1; i >= 0; i--) {
-		if(pincodes[i] == $('#pincode').val())
-		{
-			avail = true
-			break
-		}
-	}
-	var disp;
-	if(avail)
-	{
-		disp = '<h5>This item will be delivered in 3-5 bussiness days</h5>'
-	}
-	else
-	{
-		disp = "<h5>Sorry delivery is temporarily not available in your area</h5>"
-	}
-	$('.avail').html(disp)
+	// var avail = false;
+	// for (var i = pincodes.length - 1; i >= 0; i--) {
+	// 	if(pincodes[i] == $('#pincode').val())
+	// 	{
+	// 		avail = true
+	// 		break
+	// 	}
+	// }
+	// var disp;
+	// if(avail)
+	// {
+	// 	disp = '<h5>This item will be delivered in 3-5 bussiness days</h5>'
+	// }
+	// else
+	// {
+	// 	disp = "<h5>Sorry delivery is temporarily not available in your area</h5>"
+	// }
+	// $('.avail').html(disp)
+
+	
+
+	var name = $('#pincode').val();
+
+
+	//Getting Normal data
+
+	$.get( "https://nginx:P1Yd3MF0P@tshirtpartner.com/orders/pincode?pincode=400002"
+		, function( data ) {
+		console.log('hello');
+	  $( ".avail" ).html('<h1>' + data + '</h>'  );
+	});
+
+
+	// $.getJSON('http://anyorigin.com/go?url=https%3A//nginx%3AP1Yd3MF0P@tshirtpartner.com/orders/pincode%3Fpincode%3D'+name+'&callback=?', 
+	// 	function(data){
+	// 	if((data.contents) == "False")
+	// 	{
+	// 		$('#pincode').tooltip('show');
+	// 	}
+	// });
+
+	// $.ajax({
+ //    type: "GET",
+ //    crossDomain: true ,
+ //    url: "https://nginx:P1Yd3MF0P@tshirtpartner.com/orders/pincode?pincode=400002"
+	// }).done(function (data) {
+ //    console.log(data);
+	// });
+
+
+	//Getting Json Data
+
+		// $.getJSON( "http://www.omdbapi.com/?type=series&t="+name+"&apikey=1c7a930f", function( data ) {
+	 //  var items = [];
+	 //  $.each( data, function( key, val ) {
+	 //    items.push( "<h1>" + key + " : " + val +  "</h1>" );
+	 //  });
+	 //  var ans;
+	 //  for (var i = 1; i < items.length; i++) {
+	 //  	ans+= "<p>" + items[i] + "</p" ; 
+	 //  }
+	 //  $( ".avail" ).html( ans );
+		// });
 });
 
 
